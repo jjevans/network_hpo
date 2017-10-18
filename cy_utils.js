fc48185a020425af38f378590b509bbe650e80f7
jjevans <jason.j.evans@gmail.com>
2017-10-18 01:57:11 -0400

    use class 'foreground' to style nodes in foreground. fixed bug with process_cy().

--- cy_utils.js
+++ cy_utils.js
@@ -2,8 +2,10 @@
 		var highlight_color = "blue";
 		var node_bg_opacity = 0.4;
 		var node_fg_opacity = 1;
-		var lbl_fld = 'data(id)';//id, description, label fields for node label
+		var fg_class = "foreground";
+		var lbl_fld = 'data(term)';//id, term, description, label fields for node label
 		var dbltap_timelen = 300;
+		var nodes_to_fit;
 
 		function init_cy(elements){
 			var off_opacity = 0.4;//opacity of nodes not related to clicked
@@ -13,14 +15,15 @@
 				randomize: false,
 				avoidOverlap: true,
 				//nodeDimensionsIncludeLabels: true,
+				handleDisconnected: true
 				//fit: true,
 				//nodeSpacing: function( node ){ return 2; },
-				ready: function(){
+				/*ready: function(){
 					cy.fit();
 				},
 				stop: function(){
 					cy.fit();
-				}
+				}*/
 			};
 
 			var cy = cytoscape({
@@ -61,81 +64,7 @@
 				wheelSensitivity: 1
 			});	
 
-			//automove
-			am_opts = {
-				nodesMatching: function(node){ return node.descendants() },
-				reposition: 'drag'
-			};
-			//cy.automove(am_opts);
-		function process_cy(event){
-			var node_target = event.target;
-			var node_info = event.node;
-			////var node_pos_x = new Array();//all node pos for zoom, pan
-			////var node_pos_y = new Array();
-			var nodes_to_fit = new Array();
-	
-			cy.batch(function(){
-				cy.nodes()
-				.filter(function(node){return node != node_target})
-				.each(function(node){
-					node.style({opacity: node_bg_opacity, 'background-color': node_color});
-					//node.style({display: 'none'});
-				});
-			});
-					
-			node_target.style({opacity: node_fg_opacity, 'background-color': node_color});
-			//node_target.style({display: 'element', 'background-color': node_color});
-			////node_pos_x.push(node.target.position('x'));
-			////node_pos_y.push(node.target.position('y'));
-			nodes_to_fit.push(node_target);
 
-			node_target
-			.connectedEdges()
-				.each(function(edge){
-					if(edge.target() != node_target){
-						node_info.children.push({
-						term: edge.target().data('term'),
-						description: edge.target().data('description')});
-					}
-					edge.target().style({opacity: node_fg_opacity, 'background-color': node_color})
-					//edge.target().style({display: 'element', 'background-color': node_color})
-
-					////node_pos_x.push(edge.target().position('x'));
-					////node_pos_y.push(edge.target().position('y'));
-					nodes_to_fit.push(edge.target());
-				});
-					
-			cy.edges()
-				.filter(function(edge){return edge.target() == node_target})
-				.each(function(edge){
-					edge.source().style({opacity: node_fg_opacity, 'background-color': highlight_color});
-					//edge.source().style({display: 'element', 'background-color': highlight_color});
-		
-					node_info.parent_term = edge.source().data('term');
-					node_info.parent_desc = edge.source().data('description');
-				});
-	
-			//pan to center of displayed nodes
-			////var node_pos_tmp = node_pos_x.sort();
-			////var leftmost = node_pos_tmp[0];
-			////var rightmost = node_pos_tmp[-1];
-			////var pan_avg_x = Math.abs(rightmost-leftmost);
-			
-			////node_pos_tmp = node_pos_y.sort();
-			////var upper = node_pos_tmp[0];
-			////var lower = node_pos_tmp[-1];
-			////var pan_avg_y = Math.abs(upper-lower);
-			////cy.pan({x: pan_avg_x, y: pan_avg_y});
-			
-			cy.fit(nodes_to_fit, 0);
-			//cy.pan({x: node_target.position('x'), y: node_target.position('y')});
-//			cy.zoom({
-//				level: 1,
-//				position: node_target.position()
-//			});
-		
-//			cy.layout(cola).run();	
-		}
 
 			cy.ready(function(event){
 				/*cy.on('grab', 'node', function(event) {
@@ -173,7 +102,9 @@
 					}
 
 					$('#reset').click(function(event){
-						cy.nodes().each(function(node){ node.style({display: 'element', 'background-color': node_color})});
+						cy.$('.'+fg_class).removeClass(fg_class);
+						//cy.nodes().each(function(node){ node.style({display: 'element', 'background-color': node_color})});
+						cy.nodes().each(function(node){ node.style({opacity: node_fg_opacity, 'background-color': node_color}) });
 						cy.zoom(1);
 
 						cy.layout(cola).run();
@@ -187,6 +118,80 @@
 			});
 
 			return cy;
+		}
+
+		function process_cy(event){
+			var node_target = event.target;
+			var node_info = event.node;
+			////var node_pos_x = new Array();//all node pos for zoom, pan
+			////var node_pos_y = new Array();
+			nodes_to_fit = new Array();
+	
+			cy.batch(function(){
+				cy.nodes()
+				.filter(function(node){return node != node_target})
+				.each(function(node){
+					node.style({opacity: node_bg_opacity, 'background-color': node_color});
+					node.removeClass(fg_class);
+
+					//node.style({display: 'none'});
+				});
+			});
+
+			node_target.style({opacity: node_fg_opacity, 'background-color': node_color});
+			node_target.addClass(fg_class);
+			//node_target.style({display: 'element', 'background-color': node_color});
+			////node_pos_x.push(node.target.position('x'));
+			////node_pos_y.push(node.target.position('y'));
+			nodes_to_fit.push(node_target);
+
+			node_target
+			.connectedEdges()
+				.each(function(edge){
+					if(edge.target() != node_target){
+						node_info.children.push({
+						term: edge.target().data('term'),
+						description: edge.target().data('description')});
+					}
+					edge.target().style({opacity: node_fg_opacity, 'background-color': node_color});
+					edge.target().addClass(fg_class);
+					//edge.target().style({display: 'element', 'background-color': node_color})
+
+					////node_pos_x.push(edge.target().position('x'));
+					////node_pos_y.push(edge.target().position('y'));
+					nodes_to_fit.push(edge.target());
+				});
+					
+			cy.edges()
+				.filter(function(edge){return edge.target() == node_target})
+				.each(function(edge){
+					edge.source().style({opacity: node_fg_opacity, 'background-color': highlight_color});
+					//edge.source().style({display: 'element', 'background-color': highlight_color});
+					edge.source().addClass(fg_class);
+					node_info.parent_term = edge.source().data('term');
+					node_info.parent_desc = edge.source().data('description');
+				});
+	
+			//pan to center of displayed nodes
+			////var node_pos_tmp = node_pos_x.sort();
+			////var leftmost = node_pos_tmp[0];
+			////var rightmost = node_pos_tmp[-1];
+			////var pan_avg_x = Math.abs(rightmost-leftmost);
+			
+			////node_pos_tmp = node_pos_y.sort();
+			////var upper = node_pos_tmp[0];
+			////var lower = node_pos_tmp[-1];
+			////var pan_avg_y = Math.abs(upper-lower);
+			////cy.pan({x: pan_avg_x, y: pan_avg_y});
+			
+			cy.fit(nodes_to_fit, 0);
+			//cy.pan({x: node_target.position('x'), y: node_target.position('y')});
+//			cy.zoom({
+//				level: 1,
+//				position: node_target.position()
+//			});
+		
+//			cy.layout(cola).run();	
 		}
 
 		function subgraph(terms, root, num_gen){
